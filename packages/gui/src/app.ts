43db117c332dff529aa8d20b6179ecca51f780bb
Erik Vullings <erik.vullings@gmail.com>
2025-04-01 14:56:54 +0200

    Removed old dependencies

--- packages/gui/src/app.ts
+++ packages/gui/src/app.ts
@@ -1,17 +1,17 @@
-import m from 'mithril';
-import 'material-icons/iconfont/filled.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-import './css/style.css';
-import { routingSvc } from './services/routing-service';
-import { LANGUAGE, SAVED } from './utils';
-import { Languages, i18n } from './services';
+import m from "mithril";
+import "material-icons/iconfont/filled.css";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+import "./css/style.css";
+import { routingSvc } from "./services/routing-service";
+import { LANGUAGE, SAVED } from "./utils";
+import { Languages, i18n } from "./services";
 
-document.documentElement.setAttribute('lang', 'en');
+document.documentElement.setAttribute("lang", "nl");
 
 window.onbeforeunload = (e) => {
-  if (localStorage.getItem(SAVED) === 'true') return;
-  localStorage.setItem(SAVED, 'true');
+  if (localStorage.getItem(SAVED) === "true") return;
+  localStorage.setItem(SAVED, "true");
   e.preventDefault(); // This is necessary for older browsers
 };
 
@@ -22,8 +22,8 @@ i18n.addOnChangeListener((_locale: string) => {
 });
 i18n.init(
   {
-    en: { name: 'English', fqn: 'en-UK', default: true },
-    nl: { name: 'Nederlands', fqn: 'nl-NL' },
+    en: { name: "English", fqn: "en-UK", default: true },
+    nl: { name: "Nederlands", fqn: "nl-NL" },
   },
-  (window.localStorage.getItem(LANGUAGE) || 'nl') as Languages
+  (window.localStorage.getItem(LANGUAGE) || "nl") as Languages
 );
